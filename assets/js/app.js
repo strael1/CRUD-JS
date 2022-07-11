7089bb0cf21ba45a2147c7f23b766f9bf79eb228
Rafael Macedo <rafaelmacedosilva88@hotmail.com>
2022-07-11 19:52:17 -0300

    Commit inicial

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+{/* 
+<ul class="list">
+    <li>
+        <p>lista</p>
+        <span>
+            <button class="btn-warning btn-sm">Editar</button>
+            <button class="btn-danger btn-sm">Remover</button>
+        </span>
+    </li>
+</ul> 
+*/}
+
+const input = document.querySelector('#inputText'); // input 
+const btnAction = document.querySelector('#btnAction'); // button 
+const ul = document.querySelector('.list');
+
+btnAction.addEventListener('click', function(){
+    let li = document.createElement('li');
+    let p = document.createElement('p');
+    p.textContent = input.value;
+    input.value = '';
+    li.appendChild(p);
+    ul.appendChild(li);
+})
